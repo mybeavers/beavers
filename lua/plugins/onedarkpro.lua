8cc681c926fc9f7aeeb6fe94c93929af2b45502b
mybeavers <mybeavers@Cat>
2023-08-04 11:24:00 +0800

    更改: onelight主题颜色

--- lua/plugins/onedarkpro.lua
+++ lua/plugins/onedarkpro.lua
@@ -3,11 +3,17 @@ vim.cmd[[colorscheme onedark]]
 
 
 require("onedarkpro").setup({
+    colors = {
+        onelight = {
+            bg= "#fdf6e3",
+        },
+
+    },
+    
     options = {
         --支持透明主題
-      --  transparency = true,
-    },
-
+    }, 
+    
     styles = {
     types = "NONE",
     methods = "NONE",
@@ -25,9 +31,6 @@ require("onedarkpro").setup({
 
 
   },
-  options = {
-    cursorline = true
-  },
 
 })
 
@@ -35,8 +38,7 @@ local hour = tonumber(os.date("%H"))
 if hour >= 16 or hour < 10 then
     vim.api.nvim_exec("color onedark", false)
 else
-  vim.api.nvim_exec("color onelight", false)
+    vim.api.nvim_exec("color onelight", false)
 end
-
 
 
