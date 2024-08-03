5fd7d5ddfe7aab80df0427a21bf892a789d0f7c9
mybeavers <mybeavers@outlook.com>
2024-08-03 13:22:16 +0800

    修改

--- lua/plugins/Lazy.lua
+++ lua/plugins/Lazy.lua
@@ -138,9 +138,7 @@ require("lazy").setup({
             "hrsh7th/cmp-buffer",
             {
                 'L3MON4D3/LuaSnip',
-                dependencies = { "rafamadriz/friendly-snippets" },
                 config = function()
-                    require("luasnip.loaders.from_vscode").lazy_load()
                     require("luasnip.loaders.from_vscode").lazy_load({paths = "~/.config/nvim/lua/snippets" })
                     require("luasnip.loaders.from_snipmate").lazy_load({paths = "~/.config/nvim/lua/snippets"})
                 end
