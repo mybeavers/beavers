9b9b4108afb336c631893c1e4bf67884895b6017
mybeavers <mybeavers@outlook.com>
2024-08-03 15:01:58 +0800

    修改

--- lua/plugins/Lazy.lua
+++ lua/plugins/Lazy.lua
@@ -75,11 +75,6 @@ require("lazy").setup({
         "olimorris/onedarkpro.nvim",
         priority = 1000,
         config = function()
-                require("onedarkpro").setup({
-                    options = {
-                        transparency=false
-                    }
-                })
             vim.cmd("colorscheme onedark")
         end,
     },
